ea411bbfb8982f7b6e675dd863790461306b49c3
Carrenolg <carrenolg@gmail.com>
2021-08-25 16:37:16 -0500

    working on kvm

---
+++ linux/kvm-virtual-machine/script.sh
@@ -0,0 +1,12 @@
+# KVM, (kernel-based Virtual Machine) is a free and opensource 
+# virtualization platform for the Linux kernel.
+# Step 1, check if the hardware supports KVM
+$ egrep -c '(vmx|svm)' /proc/cpuinfo
+# check kvm commands
+$ sudo kvm-ok
+# if dosen't works
+$ sudo apt install cpu-checker
+# so, now we can install kvm
+$ sudo apt install -y qemu qemu-kvm libvirt-daemon libvirt-clients bridge-utils virt-manager
+# service status
+$ sudo systemctl status libvirtd
